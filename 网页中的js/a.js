bd89a4084108cdd798f615c8b0ed8adc9f93c96d
stlran <43713267+stlran@users.noreply.github.com>
2019-04-16 11:22:35 +0800

    Add files via upload

---
+++ 网页中的js/a.js
@@ -0,0 +1,5 @@
+function init () {
+  var divTag = document.getElementById('kuang')
+  divTag.setAttribute('style','height: 100px; width: 60px; background-color: yellow; position:relative; top: 200px; left: 450px;')
+}
+window.onload = init
---
+++ 网页中的js/a1.js
@@ -0,0 +1,11 @@
+function init () {
+console.log (whatEver);
+var whatEver = document.getElementById ('amao')
+console.log (whatEver)
+console.log (100/0)
+console.log (0/100)
+console.log('aa'*1)
+console.log (typeof(100))
+console.log (typeof('100'))
+}
+window.onload = init
